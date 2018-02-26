3eb5285e2859a4d4ce4a2462cf4e2d5f3ef38311
Zac Garby <me@zacgarby.co.uk>
2018-02-26 17:18:59 +0000

    to_string errors before printing them

--- lib/miner.ex
+++ lib/miner.ex
@@ -6,6 +6,7 @@ defmodule Miner do
   alias UltraDark.Transaction
   alias UltraDark.UtxoStore
   alias UltraDark.Utilities
+  alias UltraDark.Error
   alias Decimal, as: D
 
   @index_space 10
@@ -13,9 +14,9 @@ defmodule Miner do
   @elapsed_space 10
 
   def initialize(address) do
-    Ledger.initialize
-    UtxoStore.initialize
-    chain = Blockchain.initialize
+    Ledger.initialize()
+    UtxoStore.initialize()
+    chain = Blockchain.initialize()
 
     main(chain, address, List.first(chain).difficulty)
   end
@@ -23,12 +24,12 @@ defmodule Miner do
   def main(chain, address, difficulty) do
     block =
       List.first(chain)
-      |> Block.initialize
+      |> Block.initialize()
 
     difficulty =
-      if rem(block.index, Blockchain.diff_rebalance_offset) == 0 do
+      if rem(block.index, Blockchain.diff_rebalance_offset()) == 0 do
         new_difficulty = Blockchain.recalculate_difficulty(chain) + difficulty
-        IO.puts "Difficulty recalculated! Changed from #{difficulty} to #{new_difficulty}"
+        IO.puts("Difficulty recalculated! Changed from #{difficulty} to #{new_difficulty}")
         new_difficulty
       else
         difficulty
@@ -38,32 +39,38 @@ defmodule Miner do
 
     IO.write("Mining block #{block.index}...\r")
 
-    before = :os.system_time
+    before = :os.system_time()
 
     block =
       block
       |> calculate_coinbase_amount
       |> Transaction.generate_coinbase(address)
       |> merge_block(block)
-      |> Block.mine
+      |> Block.mine()
 
     blue = "\e[34m"
     clear = "\e[0m"
-    elapsed = (:os.system_time - before) / 1000000000
+    elapsed = (:os.system_time() - before) / 1_000_000_000
 
     index_str = "#{blue}Index:#{clear} #{String.pad_trailing(inspect(block.index), @index_space)}"
     hash_str = "#{blue}Hash:#{clear} #{block.hash}"
     nonce_str = "#{blue}Nonce:#{clear} #{String.pad_trailing(inspect(block.nonce), @nonce_space)}"
-    elapsed_str = "#{blue}Elapsed (s):#{clear} #{String.pad_trailing(inspect(elapsed), @elapsed_space)}"
+
+    elapsed_str =
+      "#{blue}Elapsed (s):#{clear} #{String.pad_trailing(inspect(elapsed), @elapsed_space)}"
+
     hashrate_str = "#{blue}Hashrate (H/s):#{clear} #{round(block.nonce / elapsed)}"
 
-    IO.puts "#{index_str} #{hash_str} #{nonce_str} #{elapsed_str} #{hashrate_str}"
+    IO.puts("#{index_str} #{hash_str} #{nonce_str} #{elapsed_str} #{hashrate_str}")
+
+    block = %{block | index: block.index - 1}
 
     case Validator.is_block_valid?(block, chain, difficulty) do
-      :ok -> main(Blockchain.add_block(chain, block), address, difficulty)
+      :ok ->
+        main(Blockchain.add_block(chain, block), address, difficulty)
 
-      {:error, err} ->
-        IO.puts err
+      err ->
+        IO.puts(Error.to_string(err))
         main(chain, address, difficulty)
     end
   end
@@ -75,7 +82,7 @@ defmodule Miner do
 
   defp merge_block(coinbase, block) do
     new_transactions = [coinbase | block.transactions]
-    txoids = Enum.map(new_transactions, &(&1.id))
+    txoids = Enum.map(new_transactions, & &1.id)
 
     Map.merge(block, %{
       transactions: new_transactions,
