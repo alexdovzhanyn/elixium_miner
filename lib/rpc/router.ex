9e451dccbcf720adc12abccd5c77cc72c0c38c4b
Alex Dovzhanyn <dovzhanyn.alex@gmail.com>
2018-11-19 22:11:53 -0500

    rpc show correct encoding

--- lib/rpc/router.ex
+++ lib/rpc/router.ex
@@ -11,6 +11,7 @@ defmodule Miner.RPC.Router do
         index
         |> String.to_integer()
         |> Ledger.block_at_height()
+        |> nonbinary_block()
 
       case block do
         :none -> "Block #{block} not found."
@@ -25,11 +26,13 @@ defmodule Miner.RPC.Router do
   def get("/block_by_hash/" <> hash) do
     hash
     |> Ledger.retrieve_block()
+    |> nonbinary_block()
     |> Poison.encode!()
   end
 
   def get("/latest_block") do
     Ledger.last_block()
+    |> nonbinary_block()
     |> Poison.encode!()
   end
 
@@ -38,6 +41,7 @@ defmodule Miner.RPC.Router do
       count
       |> String.to_integer()
       |> Ledger.last_n_blocks()
+      |> Enum.map(&nonbinary_block/1)
       |> Poison.encode!()
     rescue
       ArgumentError -> "Please provide an integer"
@@ -45,5 +49,12 @@ defmodule Miner.RPC.Router do
   end
 
   def get(_), do: "404"
+
+  @doc """
+    Converts binary data within a block to its non-binary equivalent
+  """
+  def nonbinary_block(%{nonce: nonce} = block) do
+    %{block | nonce: :binary.decode_unsigned(nonce)}
+  end
 
 end
