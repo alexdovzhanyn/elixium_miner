627c76267e7d37a1f2fd8ac5c46156ade3c83301
Alex Dovzhanyn <dovzhanyn.alex@gmail.com>
2018-11-23 23:03:16 -0500

    use newer version of core

--- mix.exs
+++ mix.exs
@@ -28,7 +28,7 @@ defmodule Miner.Mixfile do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:elixium_core, "~> 0.1"},
+      {:elixium_core, "~> 0.3"},
       {:decimal, "~> 1.0"},
       {:poison, "~> 3.1"},
       {:distillery, "~> 2.0"}
