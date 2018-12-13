6447ccffd96886691115d0748ef5e82d9dbccb44
Alex Dovzhanyn <dovzhanyn.alex@gmail.com>
2018-12-13 17:14:50 -0500

    bump version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Miner.Mixfile do
   def project do
     [
       app: :elixium_miner,
-      version: "1.1.2",
+      version: "1.1.3",
       elixir: "~> 1.7",
       start_permanent: true,
       deps: deps()
